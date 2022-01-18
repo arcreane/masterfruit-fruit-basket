af91a1ae85e9c01747c747ec193eb6349230e267
CyrGeo <cyrille.george@gmail.com>
2022-01-18 15:23:52 +0100

    First Structure for Mastermind game (activities + classes)

--- app/src/main/java/com/example/fruitsbasket/MainActivity.java
+++ app/src/main/java/com/example/fruitsbasket/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.fruitsbasket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button m_startButton = findViewById(R.id.StartGameBtn);
+        Button m_scoresButton = findViewById(R.id.ScoresBtn);
+        Button m_quitButton = findViewById(R.id.ExitBtn);
+
+        m_quitButton.setOnClickListener(view ->{
+            finish();
+        } );
+
+        m_scoresButton.setOnClickListener(view -> {
+            Intent scores = new Intent(MainActivity.this, Scores.class);
+            startActivity(scores);
+        });
+
+        m_startButton.setOnClickListener(view -> {
+            Intent gameplay = new Intent(MainActivity.this, GamePlay.class);
+            startActivity(gameplay);
+        });
+
     }
 }
